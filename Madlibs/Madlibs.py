455561aaac4ae8bb854255869b6bd781038d2115
Fahim Abrar <132096309+r00kieBlinder@users.noreply.github.com>
2023-08-14 11:03:28 +0600

    Initial commit

---
+++ Madlibs/Madlibs.py
@@ -0,0 +1,20 @@
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to _____ "
+# youtuber = 'Clever Programmer' # some string variable 
+
+# # a few ways to do this 
+# print ('subscirbe to' + youtuber)
+# print ('subscribe to {}'.format(youtuber))
+# print (f'subscribe to {youtuber}')
+
+
+adj1 = input('Adjective : ')
+superhero = input('Superhero : ')
+youtuber = input('Youtuber : ')
+adj2 = input('Adjective : ')
+adj3 = input('Adjective : ')
+
+madlib = f'Computer Programming is so {adj1}! I feel like I am {superhero} doing it. \
+I really appreciate {youtuber} for your {adj2} Python tutorial. I {adj3} you!'
+
+print(madlib)
